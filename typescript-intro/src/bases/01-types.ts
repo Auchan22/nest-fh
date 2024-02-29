56aae7720362519b7c5061b5b2973f9efb849cab
Auchan22 <agustinsurila@gmail.com>
2024-02-29 11:40:03 -0300

    Se continuan tipos básicos

--- typescript-intro/src/bases/01-types.ts
+++ typescript-intro/src/bases/01-types.ts
@@ -1,1 +1,7 @@
-export const name = "Agustin";
+export let name: string | number = "Agustin" ;
+export const age: number = 20;
+export const isValid: boolean = false;
+
+name = "Pepe";
+
+console.log(`Que onda ${name}? ${age}`)
--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { name } from './bases/01-types.ts'
+import { age, name } from './bases/01-types.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,7 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Bienvenido ${name}</h1>
+    <h1>Bienvenido ${name} | ${age}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
